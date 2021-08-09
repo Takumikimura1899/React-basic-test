c9fae6ee3770ef460778bce14663e092593d9f7f
Takumikimura141899 <kimura141899@gmail.com>
2021-08-09 16:49:13 +0900

    basic1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Basic1 from './components/Basic1';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Basic1 name='Hooks' />
+      <Basic1 name='Today' />
+    </div>
+  );
 }
 
 export default App;
