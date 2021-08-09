26f697630f4d42899dab9973fd649cbaddd4d947
Takumikimura141899 <kimura141899@gmail.com>
2021-08-09 21:38:21 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import ApiFetch from './components/ApiFetch';
+import B from './components/B';
 // import Basic1 from './components/Basic1';
 // import Basic2 from './components/Basic2';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
+import AppContext from './contexts/AppContext';
 
 function App() {
   return (
-    <div className='App'>
-      {/* <Basic2 /> */}
-      {/* <BasicUseEffect /> */}
-      {/* <TimerContainer /> */}
-      <ApiFetch />
-    </div>
+    <AppContext.Provider value={'value from App.js'}>
+      <div className='App'>
+        {/* <Basic2 /> */}
+        {/* <BasicUseEffect /> */}
+        {/* <TimerContainer /> */}
+        <ApiFetch />
+        <B />
+      </div>
+    </AppContext.Provider>
   );
 }
 
