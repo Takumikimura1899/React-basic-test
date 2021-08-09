0cb685ab42c4f9dbe02c2f745b8cb11bd1f58577
Takumikimura141899 <kimura141899@gmail.com>
2021-08-09 18:43:02 +0900

    useEffect CleanUp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Basic1 from './components/Basic1';
-import Basic2 from './components/Basic2';
+// import Basic1 from './components/Basic1';
+// import Basic2 from './components/Basic2';
+// import BasicUseEffect from './components/BasicUseEffect';
+import TimerContainer from './components/TimerContainer';
 
 function App() {
   return (
     <div className='App'>
-      <Basic2 />
+      {/* <Basic2 /> */}
+      {/* <BasicUseEffect /> */}
+      <TimerContainer />
     </div>
   );
 }
