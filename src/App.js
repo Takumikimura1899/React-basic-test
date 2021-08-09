bc30c6530e752741fb0bccb2657d6f4912a83b20
Takumikimura141899 <kimura141899@gmail.com>
2021-08-09 20:54:20 +0900

    axios get id

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import ApiFetch from './components/ApiFetch';
 // import Basic1 from './components/Basic1';
 // import Basic2 from './components/Basic2';
 // import BasicUseEffect from './components/BasicUseEffect';
-import TimerContainer from './components/TimerContainer';
+// import TimerContainer from './components/TimerContainer';
 
 function App() {
   return (
     <div className='App'>
       {/* <Basic2 /> */}
       {/* <BasicUseEffect /> */}
-      <TimerContainer />
+      {/* <TimerContainer /> */}
+      <ApiFetch />
     </div>
   );
 }
