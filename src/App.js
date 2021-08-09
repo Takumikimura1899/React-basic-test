7d12c474c1726bc97f7a02b46120d939239ad539
Takumikimura141899 <kimura141899@gmail.com>
2021-08-09 22:20:43 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import ApiFetch from './components/ApiFetch';
-import B from './components/B';
+// import ApiFetch from './components/ApiFetch';
+// import B from './components/B';
+import BasicReducer from './components/BasicReducer';
 // import Basic1 from './components/Basic1';
 // import Basic2 from './components/Basic2';
 // import BasicUseEffect from './components/BasicUseEffect';
@@ -14,8 +15,9 @@ function App() {
         {/* <Basic2 /> */}
         {/* <BasicUseEffect /> */}
         {/* <TimerContainer /> */}
-        <ApiFetch />
-        <B />
+        {/* <ApiFetch />
+        <B /> */}
+        <BasicReducer />
       </div>
     </AppContext.Provider>
   );
